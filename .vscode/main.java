f1b7cdceb681ef99ac8e39c272ca0789137d87c1
Kcuyapi-cmd <kcuyapi@ucvvirtual.edu.pe>
2025-09-16 08:14:54 +0000

    subir mi primer prpgrama

---
+++ .vscode/main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String nombre = "Kevin";
+        double nota = 18.5;
+
+        System.out.println("Hola " + nombre + ", tu nota es: " + nota);
+
+        double promedio = (nota + 20.0) / 2;
+        System.out.println("Tu promedio es: " + promedio);
+    }
+}
